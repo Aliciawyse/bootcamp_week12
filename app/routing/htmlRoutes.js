7a59296f39521f2db593dc1a31760343f8b7f1ed
Barrett <Aliciawyse@users.noreply.github.com>
2017-11-08 13:16:41 -0500

    work in progress

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,22 +1,24 @@
-// Dependencies
-// ===========================================================
-var express = require("express");
+var express = require('express');
+var router = express.Router();
 var path = require("path");
-var app = express();
-var PORT = process.env.PORT || 3000;
+
+
 
 // Routes
 // ===========================================================
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/home.html"));
 });
 
-app.get("/survey", function(req, res) {
+router.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/survey.html"));
 });
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+
+// router.post("/api/friends", function(req,res){
+//     console.log(req);
+//     document.write("Body!", req.body);
+//     res.send("Got info!");
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-//dependencies
-
+// Dependencies
+// ===========================================================
 var express = require("express");
+//var path = require("path");
+var app = express();
 var bodyParser = require("body-parser");
-var path = require("path");
+var PORT = process.env.PORT || 3000;
+
+var index = require("./app/routing/htmlRoutes");
+var api = require("./app/routing/apiRoutes");
+
+
+//parse data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+//routes
+app.use('/', index);
+app.use('/api', api);
+
+
+// Starts the server to begin listening
+//=============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
